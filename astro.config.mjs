033aab341691b597153550ea11b605995c6c00d5
Shin McCold <24843888+shinmccold@users.noreply.github.com>
2024-09-03 18:20:17 +0000

    added timeline view

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	
 	site: 'https://shinmccold.github.io',
-	integrations: [mdx(), sitemap(), react()],
+	integrations: [mdx(), sitemap(), tailwind(), react({
+		experimentalReactChildren: true,
+	  }),],
 });
